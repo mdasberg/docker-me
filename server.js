4df7415a877574954e04c57477f7da6a32278c51
Mischa Dasberg <mischa@dasberg.nl>
2017-05-15 20:12:17 +0200

    Add exception function for server failure

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 var proxy = (require('http-proxy')).createProxyServer();
-const path = require('path');
 const app = express();
 
 app.set('port', (process.env.PORT || 3001));
@@ -8,12 +7,15 @@ app.set('port', (process.env.PORT || 3001));
 // serve the mocking interface for local development
 app.use('/', express.static('.'));
 
-app.all("/api/*", function (req, res) {
+app.all("/api/*", function (req, res, next) {
     proxy.web(req, res, {
         target: "http://ngapimock:3000",
         secure: false,
         logLevel: "debug",
         changeOrigin: true
+    }, function (e) {
+        console.log(e)
+        next();
     });
 });
 
